ae856baf34af4fbefb781358a8071ddb0eb079b9
Rudresh <rudreshsankpal21@gmail.com>
2026-07-04 22:32:43 +0530

    learned how to do basic print in java

---
+++ demo.java
@@ -0,0 +1,7 @@
+class demo{
+    String str = "Rudresh";
+    public static void main(String cp[]){
+        demo obj = new demo();
+        System.out.println("hello World from : " + obj.str);
+    }
+}
