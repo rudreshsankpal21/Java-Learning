7ebf3af7743221b783d6817a5b398f537cd8393a
Rudresh <rudreshsankpal21@gmail.com>
2026-07-07 18:54:09 +0530

    solved a qst of constructor overloading

--- constructor.java
+++ constructor.java
@@ -1,21 +1,44 @@
 //  Constructor Overloading
+class cons {
+    protected double pi = 3.14;
 
-class cons{
     // String name;
-    cons(String fname){
-        System.out.println("Parameterized Constructor : " + fname);
+    // Area of Rectangle
+    cons(int l, int b) {
+        int res = l * b;
+        System.out.println("Area of Rectangle is : " + res);
     }
 
-    cons(){
-        System.out.print("Default Constructor");
+    // Area of Circle
+    cons(double r) {
+        double res = pi * r * r;
+        System.out.println("Area of Rectangle is : " + res);
+    }
+
+    cons(int num) {
+        if (num > 0) {
+            System.out.println("Entered number is positive number :  " + num);
+        } else {
+            System.out.println("Entered number is negative number :  " + num);
+        }
+    }
+
+    cons() {
+        System.out.println("Table of 2 using Default Constructor ");
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 
     public static void main(String[] args) {
-         new cons("Rudresh");
-         new cons();
-        
+        new cons(3, 4);
+        new cons(5);
+        new cons(5.6);
+        new cons();
+
     }
 }
 
-
-//  
+//
