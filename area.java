726f6b3ad9838c536d274ba4c047314648c24191
Rudresh <rudreshsankpal21@gmail.com>
2026-09-19 20:07:52 +0530

    area of circle using javaa

--- area.java
+++ area.java
@@ -1,20 +1,20 @@
 import java.util.*;
 class area{
-	void show(double l , double b){
-		double res = l * b;
-		System.out.println("Area of rectangle is : " + res);
+	void show(double r){
+		double res = 3.14 * r * r;
+		System.out.println("Area of circle is : " + res);
 	}
 
  public static void main(String cp[]){
 		Scanner sc = new Scanner(System.in);
 		area a = new area();
 		
-		System.out.print("Enter length :");
-		double len = sc.nextDouble();
+		System.out.print("Enter radius :");
+		double r = sc.nextDouble();
 		
-		System.out.print("Enter breadth :");
-		double br = sc.nextDouble();
+		// System.out.print("Enter breadth :");
+		// double br = sc.nextDouble();
 		
-		a.show(len , br);
+		a.show(r);
 	}
  }
